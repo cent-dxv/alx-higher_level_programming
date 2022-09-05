00f9a2526578fc04af0e3ffd4f48b781e5167efd
cent-dxv <centdxv@gmail.com>
2022-09-05 14:20:43 -0300

    re adjust

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// function that returns the reversed version of a list
-exports.esrever = function (list) {
-  const a = [];
-  for (let i = 0; i < list.length; i++) {
-    a.unshift(list[i]);
-  }
-  return a;
-};
